4359ce16ad85c4074a21d2534e7e3de03c64cb3c
DevDesktop <bigred4591@proton.me>
2025-11-01 22:51:49 -0400

    Fixed gazebo sim, added slam mapping

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -29,6 +29,14 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time' : True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
     default_world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
@@ -89,6 +97,7 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         joystick,
+        twist_mux,
         world_arg,
         gazebo,
         spawn_entity,
